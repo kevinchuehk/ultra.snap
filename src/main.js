9b179415293fadfa52953c754bc6d9a27f021a12
kevin3251 <herry3251@gmail.com>
2021-02-24 14:31:16 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ Menu.setApplicationMenu(false)
 const isProduction = !process.env.TEST
 
 const defaultProps = {
-    frame: false,
-    fullscreen: true,
+    frame: true,
+    alwaysOnTop: false,
     webPreferences: {
         devTools: false,
         sandbox: false
@@ -17,7 +17,6 @@ const defaultProps = {
 class Window extends BrowserWindow {
     constructor({ url, ...windiwSettings }) {
         super({ ...defaultProps, ...windiwSettings })
-        // this.setMenuBarVisibility(false)
         this.loadURL(url)
 
         this.once('ready-to-show', () => {
@@ -29,14 +28,10 @@ class Window extends BrowserWindow {
 const width = process.env.ULTRA_W || 1280
 const height = process.env.ULTRA_H || 720
 const url = process.env.ULTRA_URL || ''
-const isFullscreen = () => (width && height) ? false : true 
-const isKiosk = isFullscreen
 
 app.on('ready', () => {
     const mainWindow = new Window({
         url: url || 'https://ultra.ypcloud.com',
-        fullscreen: isFullscreen(),
-        kiosk: isKiosk(),
         width, height
     })
     app.focus()
